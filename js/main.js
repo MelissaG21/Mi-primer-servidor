9c8592815276c6cadc43fef3b613dbcec3166ffa
moonormal <a334259@uach.mx>
2021-05-14 00:44:08 -0600

    Se agrego main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let input = document.getElementById("email");
+input.value = "luis@uach.mx";
+
+let button = documento.getElementById("show");
+
+function mostrarPassword(){
+    let password = document.getElementById("pass");
+    password.type='password';
+}
+
